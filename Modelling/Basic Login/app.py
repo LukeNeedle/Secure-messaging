c79d0d335ef5d9f289ccce4805ab9e57a854f17c
Luke Needle <crazyplopl17@gmail.com>
2023-09-20 21:16:42 +0100

    Modelling login

--- Modelling/Basic Login/app.py
+++ Modelling/Basic Login/app.py
@@ -44,62 +44,111 @@ def entryCleaner(entry, mode="sql"):
     else:
         raise f"Invalid mode: {mode} for entryCleaner"
 
+def myHashFunction(variable, mode="password"):
+    """
+    Hashes the variable that is passed in.
+
+    Args:
+        variable (string): Any variable that needs hashing.
+        mode (str, optional): _description_. Defaults to "password".
+
+    Returns:
+        The hashed variable
+    """
+
+    hashedVariable = variable.encode()
+    return hashedVariable
+
 @login_manager.user_loader
-def user_loader(email):
-    # Load user from the database based on the email
-    # Replace this with your own logic
+def user_loader():
+    email = request.form.get('email')
+    cleanedEmail = entryCleaner(entry=email, mode="sql")
 
-    user = User(email)
-    return user
-
-@login_manager.request_loader
-def request_loader(request):
-    username = request.form.get('name')
-    cleanedUsername = entryCleaner(username, "sql")
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    cursor.execute(f"""SELECT * FROM Staff WHERE ;""")
+
+    cursor.execute(f"""SELECT * FROM Staff WHERE Email='{cleanedEmail}';""")
     result = cursor.fetchone()
     if result == None:
-        raise "User not found"
+        connection.close()
+        return None
     else:
-
-        (1, 'John', 'Smith', 'Mr', 'JS@school.uk', 'False', 'False', 'Averysecurepasswordthathasbeenhashed', 'Arandomstringofcharacters', 'True', 'False', 'False')
-    userDetails = {
-        "id": result[0],
-        "title": result[1],
-        "firstName": result[2],
-        "lastName": result[3],
-        "email": result[4]
-        "accountEnabled": result[5],
-        "accountArchived": result[6],
-        "password": result[7],
-        "passhash": result[8],
-        "SENCo": result[9],
-        "safeguarding": result[10],
-        "admin": result[11]
-    }
-
-    # Load user from the database based on the email
-    # Replace this with your own logic
-    user = User()
+        userDetails = {
+            "id": result[0],
+            "title": result[1],
+            "firstName": result[2],
+            "lastName": result[3],
+            "email": result[4],
+            "accountEnabled": result[5],
+            "accountArchived": result[6],
+            "password": result[7],
+            "passhash": result[8],
+            "SENCo": result[9],
+            "safeguarding": result[10],
+            "admin": result[11]
+        }
+        user = User(userDetails['id'], userDetails['title'], userDetails['firstName'], userDetails['lastName'], userDetails['accountEnabled'], userDetails['accountArchived'], userDetails['password'], userDetails['passhash'], userDetails['SENCo'], userDetails['safeguarding'], userDetails['admin'])
+    connection.close()
     return user
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    # Validate the email and password
-    # Replace this with your own validation logic
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    # Authenticate the user
-    user = User(email)
-    login_user(user, remember=True)
-    return redirect(url_for('main.profile'))
+        cleanedEmail = entryCleaner(email)
+        cleanedPassword = entryCleaner(password)
+
+        if cleanedEmail != email:
+            # Invalid email
+            return redirect(url_for('login'))
+        if cleanedPassword != password:
+            # Invalid password
+            return redirect(url_for('login'))
+        
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        
+        cursor.execute(f"""SELECT passHash FROM Staff WHERE Email='{cleanedEmail}';""")
+        result = cursor.fetchone()
+        if result == None:
+            connection.close()
+            # User not found
+            return redirect(url_for('login'))
+        
+        if result == myHashFunction(cleanedPassword):
+            cursor.execute(f"""SELECT * FROM Staff WHERE Email='{cleanedEmail}';""")
+            result = cursor.fetchone()
+            if result == None:
+                connection.close()
+                return None
+            else:
+                userDetails = {
+                    "id": result[0],
+                    "title": result[1],
+                    "firstName": result[2],
+                    "lastName": result[3],
+                    "email": result[4],
+                    "accountEnabled": result[5],
+                    "accountArchived": result[6],
+                    "password": result[7],
+                    "passhash": result[8],
+                    "SENCo": result[9],
+                    "safeguarding": result[10],
+                    "admin": result[11]
+                }
+                user = User(userDetails['id'], userDetails['title'], userDetails['firstName'], userDetails['lastName'], userDetails['accountEnabled'], userDetails['accountArchived'], userDetails['password'], userDetails['passhash'], userDetails['SENCo'], userDetails['safeguarding'], userDetails['admin'])
+                login_user(user, remember=True)
+        connection.close()
+        return redirect(url_for('profile'))
 
 @app.route('/logout')
 @login_required
@@ -110,8 +159,7 @@ def logout():
 @app.route('/profile')
 @login_required
 def profile():
-    # Only logged-in users can access this route
-    return 'Profile Page'
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
