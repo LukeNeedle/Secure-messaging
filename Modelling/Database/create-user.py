8473ff513b72a902b4c45ff309cc5309a4290cb3
LukeNeedle <112342734+LukeNeedle@users.noreply.github.com>
2023-09-21 12:19:48 +0100

    Update create-user.py
    Turned the code into a function so that it can be called to by a different program.

--- Modelling/Database/create-user.py
+++ Modelling/Database/create-user.py
@@ -1,38 +1,48 @@
 import sqlite3
+def create_user(firstName:str, lastName:str, title:str, email:str,
+                enabled:bool, SENCo:bool, safeguarding:bool, admin:bool,
+                passwordHash, passwordSalt):
+    """
+    Creates the user from the data provided. Doesn't return anything.
 
-conn = sqlite3.connect("database.db")
-cur = conn.cursor()
+    Args:
+        firstName (str): The staff member's first name
+        lastName (str): The staff member's last name
+        title (str): The staff member's title
+        email (str): The staff member's email
+        enabled (bool): Whether the staff member can login
+        SENCo (bool): Whether they are a member of the SENCo team
+        safeguarding (bool): Whether they are a member of the safeguarding team
+        admin (bool): Whether the staff member have admin access
+        passwordHash (blob): The hashed password
+        passwordSalt (blob): The random salt used to hash the password
+    """
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
 
-#Example Values
-firstName = "John"
-lastName = "Smith"
-title = "Mr"
-email = "JS@school.uk"
-enabled = "False"
-SENCo = True
-safeguarding = False
-admin = False
-passwordHash = "Averysecurepasswordthathasbeenhashed".encode() # Will be passed in already encoded
-passwordSalt = "Arandomstringofcharacters".encode() # Will be passed in already encoded    
-#
+    try:
+        cur.execute(f"""INSERT INTO Staff(FirstName, LastName, Title, Email, AccountEnabled, AccountArchived, PassHash, PassSalt, SENCo, Safeguarding, Admin)
+                    VALUES ('{firstName}', '{lastName}', '{title}', '{email}', '{enabled}', 'False', '{passwordHash.decode()}', '{passwordSalt.decode()}', '{SENCo}', '{safeguarding}', '{admin}');""")
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("Failed CHECK constraint")
 
-try:
-    cur.execute(f"""INSERT INTO Staff(FirstName, LastName, Title, Email, AccountEnabled, AccountArchived, PassHash, PassSalt, SENCo, Safeguarding, Admin)
-                VALUES ('{firstName}', '{lastName}', '{title}', '{email}', '{enabled}', 'False', '{passwordHash.decode()}', '{passwordSalt.decode()}', '{SENCo}', '{safeguarding}', '{admin}');""")
-    conn.commit()
-except sqlite3.IntegrityError:
-    print("Failed CHECK constraint")
+    cur.execute(f"""SELECT StaffID FROM Staff WHERE FirstName='{firstName}' and LastName='{lastName}' and Title='{title}' and AccountEnabled='{enabled}' and SENCo='{SENCo}' and Safeguarding='{safeguarding}' and Admin='{admin}'""")
+    results = cur.fetchall()
 
-cur.execute(f"""SELECT StaffID FROM Staff WHERE FirstName='{firstName}' and LastName='{lastName}' and Title='{title}' and AccountEnabled='{enabled}' and SENCo='{SENCo}' and Safeguarding='{safeguarding}' and Admin='{admin}'""")
-results = cur.fetchall()
+    if len(results) > 1:
+        print("Too many other entries, take largest")
+        staffID = results[len(results)-1][0]
+    elif len(results) == 1:
+        staffID = results[0][0]
+    elif len(results) == 0:
+        conn.close()
+        raise "Error: Staff member doesn't exist"
 
-if len(results) > 1:
-    print("Too many other entries, take largest")
-    staffID = results[len(results)-1][0]
-elif len(results) == 1:
-    staffID = results[0][0]
-elif len(results) == 0:
-    raise "Error: Staff doesn't exist"
+    conn.close()
+    return staffID
 
-
-conn.close()
+create_user(firstName="John", lastName="Smith", title="Mr", email = "JS@school.uk",
+            enabled = "False", SENCo = True, safeguarding = False, admin = False,
+            passwordHash = "Averysecurepasswordthathasbeenhashed".encode(),
+            passwordSalt = "Arandomstringofcharacters".encode())
