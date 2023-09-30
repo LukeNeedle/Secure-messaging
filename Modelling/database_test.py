64372c4ae9fbece5e424db8ce75b7ec5346bcec5
Luke Needle <crazyplopl17@gmail.com>
2023-09-30 21:55:42 +0100

    Fixed database test code
    now passes in all data in their correct types

--- Modelling/database_test.py
+++ Modelling/database_test.py
@@ -5,4 +5,4 @@ database.create_tables()
 database.create_user(firstName="John", lastName="Smith", title="Mr", email = "JS@school.uk",
             enabled = "False", SENCo = True, safeguarding = False, admin = False,
             passwordHash = "Averysecurepasswordthathasbeenhashed".encode(),
-            passwordSalt = "Arandomstringofcharacters".encode())
+            passwordSalt = "Arandomstringofcharacters")
