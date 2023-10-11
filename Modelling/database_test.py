9295582b6a69e4d7d5e1188f09d5d17705abf9b4
LukeNeedle <crazyplopl17@gmail.com>
2023-10-11 10:04:50 +0100

    Updated test database

--- Modelling/database_test.py
+++ Modelling/database_test.py
@@ -2,7 +2,7 @@ import database
 
 database.create_tables()
 
-database.create_user(firstName="John", lastName="Smith", title="Mr", email = "JS@school.uk",
+database.create_user(firstName="John", lastName="Smith", title="Mr", email = "js@school.uk",
             enabled = "False", SENCo = True, safeguarding = False, admin = False,
             passwordHash = "Averysecurepasswordthathasbeenhashed",
             passwordSalt = "Arandomstringofcharacters")
