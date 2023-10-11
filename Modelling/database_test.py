cbf8859db24fe5d0f39863b04610044caedf4f89
LukeNeedle <crazyplopl17@gmail.com>
2023-10-11 10:16:27 +0100

    Fixed test database

--- Modelling/database_test.py
+++ Modelling/database_test.py
@@ -1,8 +1,9 @@
 import database
+import hash_function
 
 database.create_tables()
 
 database.create_user(firstName="John", lastName="Smith", title="Mr", email = "js@school.uk",
             enabled = "False", SENCo = True, safeguarding = False, admin = False,
-            passwordHash = "Averysecurepasswordthathasbeenhashed",
+            passwordHash = hash_function.hash_variable("Averysecurepasswordthathasbeenhashed", "Arandomstringofcharacters"),
             passwordSalt = "Arandomstringofcharacters")
