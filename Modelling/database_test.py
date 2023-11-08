31fb0c3bc09c161a2bb733c993c756668fd3c3da
LukeNeedle <crazyplopl17@gmail.com>
2023-11-08 09:37:28 +0000

    Updated database test to include an admin account

--- Modelling/database_test.py
+++ Modelling/database_test.py
@@ -7,3 +7,7 @@ database.create_user(firstName="John", lastName="Smith", title="Mr", email = "js
             enabled = True, SENCo = True, safeguarding = False, admin = False,
             passwordHash = hash_function.hash_variable("Averysecurepasswordthathasbeenhashed", "Arandomstringofcharacters"),
             passwordSalt = "Arandomstringofcharacters")
+database.create_user(firstName="A", lastName="Admin", title="Mr", email = "admin@school.uk",
+            enabled = True, SENCo = False, safeguarding = False, admin = True,
+            passwordHash = hash_function.hash_variable("password", "SALT"),
+            passwordSalt = "SALT")
