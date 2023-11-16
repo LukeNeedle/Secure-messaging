0dfc8af2122c7cdc3854736d43148ff396945994
LukeNeedle <112342734+LukeNeedle@users.noreply.github.com>
2023-11-16 19:58:47 +0000

    Fixed message decrypt

--- app.py
+++ app.py
@@ -310,11 +310,12 @@ def messages_inbox():# TODO
         
         mail = str(
             encryption.decrypt(
-                cipherText=message[3].replace("<Double_Quote>", "\"").replace("<Single_Quote>", "\'").replace("<Escape>", "\\").replace("<New_Line>", "\n").replace("<Tab>", "\t").replace("<Carriage_Return>", "\r").replace("<Null_Character>", "\0").replace("<ASCII_Bell>", "\a").replace("<ASCII_Backspace>", "\b").replace("<ASCII_Form_Feed>", "\f").replace("<ASCII_Form_Feed>", "\f").replace("<ASCII_Vertical_Tab>", "\v"),
+                cipherText=message[3].replace("<Double_Quote>", "\"").replace("<Single_Quote>", "\'").replace("<Escape>", "\\").replace("<New_Line>", "\n").replace("<Tab>", "\t").replace("<Carriage_Return>", "\r").replace("<Null_Character>", "\0").replace("<ASCII_Bell>", "\a").replace("<ASCII_Backspace>", "\b").replace("<ASCII_Form_Feed>", "\f").replace("<ASCII_Vertical_Tab>", "\v"),
                 vernamKey=str(key[:-2]),
                 subsitutionKey=int(key[-2:])
                 ).replace("\\S\\C", ';')
-            ).strip().replace("\n", ' ')
+            ).strip().replace("\n", ' ').replace("\0", '').replace("\a", '')
+        
         if len(mail) > 30:
             tempResponse.append(mail[:30])
         elif len(mail) <= 30:
